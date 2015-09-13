7baf2eafd9e3a6e92a0e4663024464d020625a35
Karsten Schulz <github@karstenschulz.biz>
2015-09-13 13:56:47 +0200

    * implemented get_add_url

--- twodolib/urlhelper.py
+++ twodolib/urlhelper.py
@@ -1,8 +1,21 @@
 """
-Module to generate URLs to manage tasks, checlists and projects in 2Do App.
+Module to generate URLs to manage tasks, checlists and projects in 2DoApp.
+
+see http://www.2doapp.com
+
 """
+
+import urllib
 
 showall_url = 'twodo://x-callback-url/showAll'
 showtoday_url = 'twodo://x-callback-url/showToday'
 showstarred_url = 'twodo://x-callback-url/showStarred'
 showscheduled_url = 'twodo://x-callback-url/showScheduled'
+
+
+_base_url = 'twodo://x-callback-url/add?{}'
+
+
+def get_add_url(task_title):
+    """Return a url to add the task, represented by the arguments."""
+    return _base_url.format('task=' + urllib.quote(task_title))
