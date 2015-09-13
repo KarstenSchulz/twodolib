b062889ab197b900965b3436373cc1c8beb8aea5
Karsten Schulz <github@karstenschulz.biz>
2015-09-13 13:26:23 +0200

    * added doc comments to tests

--- twodolib/tests.py
+++ twodolib/tests.py
@@ -7,17 +7,21 @@ class TestShowUrls(unittest.TestCase):
     """Test the 4 urls to show several lists in 2Do App."""
 
     def test_showall_url(self):
+        """Check, that showall url is correct."""
         showall_url = 'twodo://x-callback-url/showAll'
         self.assertEqual(twodolib.showall_url, showall_url)
 
     def test_showtoday_url(self):
+        """Check, that showtoday url is correct."""
         showtoday_url = 'twodo://x-callback-url/showToday'
         self.assertEqual(twodolib.showtoday_url, showtoday_url)
 
     def test_showstarred_url(self):
+        """Check, that showstarred url is correct."""
         showstarred_url = 'twodo://x-callback-url/showStarred'
         self.assertEqual(twodolib.showstarred_url, showstarred_url)
 
     def test_showscheduled_url(self):
+        """Check, that showscheduled url is correct."""
         showscheduled_url = 'twodo://x-callback-url/showScheduled'
         self.assertEqual(twodolib.showscheduled_url, showscheduled_url)
