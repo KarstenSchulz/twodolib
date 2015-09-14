acea3384fcc38689545acb2816d97822c0077175
Karsten Schulz <github@karstenschulz.biz>
2015-09-14 23:32:58 +0200

    * fixed imports

--- twodolib/__init__.py
+++ twodolib/__init__.py
@@ -3,11 +3,11 @@ from __future__ import unicode_literals
 
 __version__ = '0.0.1'
 
-from urlhelper import showall_url  # noqa
-from urlhelper import showtoday_url  # noqa
-from urlhelper import showstarred_url  # noqa
-from urlhelper import showscheduled_url  # noqa
+from .urlhelper import showall_url  # noqa
+from .urlhelper import showtoday_url  # noqa
+from .urlhelper import showstarred_url  # noqa
+from .urlhelper import showscheduled_url  # noqa
 
-from urlhelper import get_add_url  # noqa
+from .urlhelper import get_add_url  # noqa
 
-from urlhelper import TwoDoTask  # noqa
+from .urlhelper import TwoDoTask  # noqa
