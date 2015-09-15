3ab630972841e8b4f9f7a7440090169e398ddd34
Karsten Schulz <github@karstenschulz.biz>
2015-09-15 12:32:56 +0200

    ignoring external docs

--- docs/conf.py
+++ docs/conf.py
@@ -84,7 +84,7 @@ release = twodolib.__version__
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'external']
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
