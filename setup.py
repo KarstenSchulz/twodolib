7e9ca12a517a574a12a1c3328e2b59469bb4d4af
Karsten Schulz <github@karstenschulz.biz>
2017-05-28 14:10:29 +0200

    Bump version: 0.3.0 → 0.3.1

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ test_requirements = [
 
 setup(
     name='twodolib',
-    version='0.3.0',
+    version='0.3.1',
     description="Functions to manage the 2DoApp from the command line.",
     long_description=readme + '\n\n' + history,
     author="Karsten Schulz",
