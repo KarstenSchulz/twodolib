743b4504f9c5e950fe87095e0b16b6aa0cb39a57
Karsten Schulz <github@karstenschulz.biz>
2018-11-14 10:45:59 +0100

    Bump version: 0.5.0 → 0.5.1

--- twodolib/__init__.py
+++ twodolib/__init__.py
@@ -1,6 +1,6 @@
 """Modules and functions to add tasks from the commandline to the 2Do App."""
 
-__version__ = '0.5.0'
+__version__ = '0.5.1'
 
 from .urlhelper import showall_url  # noqa
 from .urlhelper import showtoday_url  # noqa
