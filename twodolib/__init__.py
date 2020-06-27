97ccbc9e2e0963249d1edfe17298c1374ebd5a34
Karsten Schulz <github@karstenschulz.biz>
2020-06-27 14:02:22 +0200

    Explain module usage in help text

--- twodolib/__init__.py
+++ twodolib/__init__.py
@@ -1,4 +1,26 @@
-"""Modules and functions to add tasks from the commandline to the 2Do App."""
+"""Modules and functions to add tasks to the 2Do App.
+
+Create a task from python::
+
+    >>> from twodolib import TwoDoTask
+    >>> my_task = TwoDoTask('Save the world.', priority=TwoDoTask.PRIO_HIGH)
+
+    >>> my_task.url()
+    u'twodo://x-callback-url/add?task=Save%20the%20world.&priority=3'
+
+Create a task, save it to 2DoApp and retrieve the task id:
+
+    >>> my_task = TwoDoTask('Save the world.', priority=TwoDoTask.PRIO_HIGH, for_list="inbox")
+
+    >>> # save the task in 2DoApp
+    >>> import subprocess
+    >>> subprocess.call(['open', my_task.url()])
+    0
+
+    >>> # retrieve taskid
+    >>> my_task.get_taskid()
+   '181378c217dd4f029a6ec7a47e65550d'
+"""
 
 __version__ = '0.5.4'
 
